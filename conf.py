5a5ce1df9feb56402cfccc71c7de31f8bd44b120
thiagontnx <toliveira@nutanix.com>
2021-04-27 12:29:50 -0400

    Remove unneeded sphinx conf line

--- conf.py
+++ conf.py
@@ -18,10 +18,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
 import sphinx_bootstrap_theme
 import sphinx_fontawesome
-
 
 
 # -- General configuration ------------------------------------------------
